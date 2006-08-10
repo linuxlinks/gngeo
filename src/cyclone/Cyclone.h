ad83347be1a6aeafe8c3f3bd05bc3ded5a38cda7
pepone <pepone@localhost>
2006-08-10 20:38:37 +0200

    [svn r14] update cyclone to version 0.0086

--- src/cyclone/Cyclone.h
+++ src/cyclone/Cyclone.h
@@ -24,8 +24,7 @@ struct Cyclone
   unsigned char irq;   // [r7,#0x47] IRQ level
   unsigned int osp;    // [r7,#0x48] Other Stack Pointer (USP/SSP)
   unsigned int vector; // [r7,#0x4c] IRQ vector (temporary)
-  int (*InvalidOpCallback)(unsigned short opcode);             // [r7,#0x50] - optional invalid op call back
-  int (*IrqCallback)(int int_level);          // [r7,#0x54] - optional irq callback function
+  unsigned int pad1[2];
   int stopped;         // [r7,#0x58] 1 == processor is in stopped state
   int cycles;          // [r7,#0x5c]
   int membase;         // [r7,#0x60] Memory Base (ARM address minus 68000 address)
@@ -39,9 +38,20 @@ struct Cyclone
   unsigned char  (*fetch8 )(unsigned int a);  // [r7,#0x80]
   unsigned short (*fetch16)(unsigned int a);  // [r7,#0x84]
   unsigned int   (*fetch32)(unsigned int a);  // [r7,#0x88]
+  void (*IrqCallback)(int int_level);         // [r7,#0x8c] - optional irq callback function, see config.h
+  void (*ResetCallback)();                    // [r7,#0x90] - if enabled in config.h, calls this whenever RESET opcode is encountered.
+  int  (*UnrecognizedCallback)();             // [r7,#0x94] - if enabled in config.h, calls this whenever unrecognized opcode is encountered.
 };
 
+// used only if Cyclone was compiled with compressed jumptable, see config.h
+void CycloneInit();
+
+// run cyclone. Cycles should be specified in context (pcy->cycles)
 void CycloneRun(struct Cyclone *pcy);
+
+// utility functions to get and set SR
+void CycloneSetSr(struct Cyclone *pcy, unsigned int sr); // auto-swaps a7<->osp if detects supervisor change
+unsigned int CycloneGetSr(struct Cyclone *pcy);
 
 #ifdef __cplusplus
 } // End of extern "C"
